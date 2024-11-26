14b8e6437b6f791aad172acfcf643d31c8a293bf
VladGurzan <vitalo2233@mail.ru>
2024-11-26 22:51:36 +0500

    Task5

--- Tyuiu.GurzanVM.Sprint5.Task5.V8/Program.cs
+++ Tyuiu.GurzanVM.Sprint5.Task5.V8/Program.cs
@@ -26,7 +26,7 @@ namespace Tyuiu.GurzanVM.Sprint5.Task4.V19
               
             DataService ds = new DataService();
 
-            string path = Path.Combine(new string[] { "C:", "DataSprint5", "InPutDataFileTask5V8.txt " });
+            string path = Path.Combine(new string[] { "C:", "DataSprint5", "InPutDataFileTask5V8.txt" });
             Console.WriteLine("Данные находятся в файле:" + path);
 
             Console.WriteLine("***************************************************************************");
